51151eb5d9f107c59b84ce680e58e7058a9782b2
Smarty Indonesia <smartyindo@gmail.com>
2026-03-18 12:09:59 +0700

    update year

--- api/footer.php
+++ api/footer.php
@@ -1,6 +1,6 @@
 <footer class="footer text-faded text-center py-5">
     <div class="container">
-      <p class="m-0 small"><strong>&copy; 2018<script>new Date().getFullYear()>2012&&document.write("-"+new Date().getFullYear());</script> | Smarty Indonesia</strong></p>
+      <p class="m-0 small"><strong>&copy; 2018-2024 | Smarty Indonesia</strong></p>
     </div>
   </footer>
 
